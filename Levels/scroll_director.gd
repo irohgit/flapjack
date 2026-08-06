621a88eef756c2f72931ec53c1a34fa62c49905f
DestroyerMob <ethanhellyer7@gmail.com>
2026-08-06 17:51:52 +1000

    hooked up to a very basic level script

--- Levels/scroll_director.gd
+++ Levels/scroll_director.gd
@@ -8,6 +8,15 @@ extends Node
 
 @export var scroll_speed := 1.0
 
+func moving() -> bool:
+	return camera.global_position != end_point
+
+func change_move_speed(speed: float) -> void:
+	scroll_speed = speed
+
+func move_to(pos: Vector2) -> void:
+	end_point = pos
+
 func _physics_process(delta: float) -> void:
 	if camera.global_position != end_point:
 		camera.global_position = camera.global_position.move_toward(end_point, delta * scroll_speed)
