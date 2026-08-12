9d33606b5efe4fc409de131e7bb3ddcd82068219
irohgit <tuantrietn@gmail.com>
2026-08-12 14:52:22 +1000

    Add: Swimming Fish Animation

---
+++ Data/Environment/bg_layer_1_spawn_data.gd
@@ -0,0 +1,8 @@
+class_name Background_Layer_1_SpawnData
+extends Resource
+
+@export var scene: PackedScene
+@export var weight: float = 1.0
+
+@export var min_scale: float = 1.0
+@export var max_scale: float = 1.0
