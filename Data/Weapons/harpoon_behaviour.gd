e102269b1a7091c2e6b4122049a48a85fede7948
DestroyerMob <ethanhellyer7@gmail.com>
2026-08-21 14:41:22 +1000

    Annoyingly not working harpoon

---
+++ Data/Weapons/harpoon_behaviour.gd
@@ -0,0 +1,35 @@
+class_name HarpoonBehaviour
+extends WeaponBehaviour
+
+func fire(shooter: Node2D, weapon: WeaponState) -> void:
+	var shot := projectile_scene.instantiate() as Projectile
+
+	if shot == null:
+		push_error("Harpoon projectile scene is not a Projectile")
+		return
+		
+	for augment in weapon.augments:
+		if augment.augmentEffect != null:
+			augment.augmentEffect.apply_to_projectile(shot)
+
+	shot.data = weapon.data.weaponProjectile
+	
+	var target := shot._find_nearest_enemy(500)
+
+	if target == null:
+		return
+
+
+	var desired_direction := shot.global_position.direction_to(
+		target.global_position
+	)
+		
+	var angle_to_target := shot.direction.angle_to(desired_direction)
+		
+	shot.direction = shot.direction.rotated(angle_to_target).normalized()
+
+
+	shooter.get_parent().add_child(shot)
+	shot.global_position = shooter.global_position + Vector2(0, -40)
+	#SFX
+	_play_fire_sound(weapon)
